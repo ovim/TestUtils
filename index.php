de88e7655157acc6773efe94ec174b1bfc0be9c1
汪玉龙 <2556811689@qq.com>
2022-05-05 17:51:39 +0800

    init

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+use Ovim\TestUtils\Test;
+
+require 'vendor/autoload.php';
+
+$result = Test::getContent();
+
+var_dump($result);
---
+++ src/Test.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ovim\TestUtils;
+
+/**
+ * test
+ */
+class Test {
+    
+    public static function getContent() {
+
+        return 'This is a test content';
+    }
+}
+
